902fbd178a839e7472817bc7b4c0f4adf292e0db
hiromi <hiromi.ogawa.1223@gmail.com>
2021-10-17 14:54:14 +0900

    laravel 構築

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // BooksTableSeederを読み込むように指定
+        $this->call(BooksTableSeeder::class);
     }
 }
